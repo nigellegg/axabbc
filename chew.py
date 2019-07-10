152c1f4ed0221d4d0c7f563448f9821ac3e8b8f0
Nigel Legg <nigel.legg@freemavens.com>
2019-07-10 14:46:28 +0100

    buggy!!

--- chew.py
+++ chew.py
@@ -6,13 +6,11 @@ import time
 from subprocess import check_output
 import sys
 from bs4 import BeautifulSoup
+import spacy
 
 feed_name = 'BBC News'
 url = 'http://feeds.bbci.co.uk/news/england/london/rss.xml'
-
-db = 'https://axabbcntl.s3.eu-west-2.amazonaws.com/feed_results.txt'
-limit = 12 * 3600 * 1000
-
+db = 'feed_results.txt'
 #
 # function to get the current time
 #
@@ -36,46 +34,65 @@ def post_is_in_db_with_old_timestamp(link):
                 if timestamp - ts > limit:
                     return True
     return False
-
+ 
 def main():
     feed = feedparser.parse(url)
     posts_to_scrape = []
     posts_to_skip = []
 
+    nlp = spacy.load("en_core_web_sm")
+    print(feed.entries)
     for post in feed.entries:
-        # if post is already in the database, skip it
-        # TODO check the time
         link = post.id
+        title = post.title
+        save =  [link,title]
         if post_is_in_db_with_old_timestamp(link):
-            posts_to_skip.append(link)
+            posts_to_skip.append(save)
         else:
-            posts_to_scrape.append(link)
-    
-    #
-    # add all the posts we're going to print to the database with the current timestamp
-    # (but only if they're not already in there)
-    #
-    f = open(db, 'a')
-    for link in posts_to_print:
-        if not post_is_in_db(link):
-            f.write(link + "|" + str(timestamp) + "\n")
-    f.close
-    
-    count = 1
-    blockcount = 1
-    for link in posts_to_scrape:
-        page = requests.get(link)
+            posts_to_scrape.append(save)
+    print(posts_to_scrape)
+    output = []
+    for item in posts_to_scrape:
+        story = {}
+        page = requests.get(item[0])
         soup = BeautifulSoup(page.text, 'html.parser')
         news_text = soup.find_all('p')
-        out = open(link+'.txt', 'w')
-        for x in news_text:
+        text = ''
+        for x in news_text: 
             x = str(x)
             s = x.find('>')
             y = x[s+1:]
-            z = y[:-4]
-            out.write(z)
-        out.close()
+            s = y.find('</')
+            z = y[:s]
+            print(z)
+            text = text + z
 
+        story['title'] = item[1]
+        lp = spacy.load("en_core_web_sm")
+        loc_url = "https://nominatim.openstreetmap.org/search?q="
+        doc = nlp(text)
+        print(doc.ents)
+        for ent in doc.ents:
+            if ent.label_ == 'location':
+                getlatlon = locurl + ent.text
+                response = requests.get(getlatlon)
+                data = response.json()
+                x = data.find('lat')
+                dat =  data[x+7:]
+                y = dat.find("'")
+                lat = dat[:y+1]
+                x = data.find('lon')
+                dat =  data[x+7:]
+                y = dat.find("'")
+                lon = dat[:y+1]
+                story['ent'] = ent,text, ent.label_
+                story['ent']['lat'] = lat
+                story['ent']['lon'] = lon                
+            else:
+                story['ent'] = ent.text, ent.label_
+            output.append(story)
+    out = open('output.json', 'w')
+    out.write(str(output))
 
 if __name__ == '__main__':
     main()
